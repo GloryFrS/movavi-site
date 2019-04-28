f00ef320e2978c4b069f1ac4517bfa5e67990633
gloryfrs <gloryfrs@yandex.ru>
2019-04-28 15:43:44 +0700

    55

--- js/common.js
+++ js/common.js
@@ -1,5 +1,11 @@
 $(function() {
 
-	// Custom JS
+	$('.panel-collapse').on('show.bs.collapse', function () {
+		$(this).siblings('.panel-heading').addClass('active');
+	  });
+	
+	  $('.panel-collapse').on('hide.bs.collapse', function () {
+		$(this).siblings('.panel-heading').removeClass('active');
+	  });
 
 });
